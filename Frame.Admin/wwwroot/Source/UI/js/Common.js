b272d98c82d44c68d89d1ec11b8e53d7d8b7b4ee
mayu <1435473873@qq.com>
2019-05-05 17:59:59 +0800

    编辑导航栏样式

--- Frame.Admin/wwwroot/Source/UI/js/Common.js
+++ Frame.Admin/wwwroot/Source/UI/js/Common.js
@@ -1,1 +1,16 @@
-﻿
+﻿//监听滚动条
+$(document).scroll(function () {
+    var wWidth = $(document).width();
+    if (~~wWidth > 768) {
+        var scroH = $(document).scrollTop();
+        $("nav").css("background", "rgba(255,255,255," + scroH / 48 + ")");
+    }
+});
+
+$(document).on("click", ".menubtn", function (e) {
+    $(".phonemenu ul").toggle();
+})
+
+$(function () {
+
+});
