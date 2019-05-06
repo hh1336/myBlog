bc0fdd2a69bc97ef4b88869b8fd3d83e52daf13e
mayu <1435473873@qq.com>
2019-05-06 17:58:44 +0800

    制作前端样式

--- Frame.Admin/wwwroot/Source/UI/js/Common.js
+++ Frame.Admin/wwwroot/Source/UI/js/Common.js
@@ -4,13 +4,28 @@ $(document).scroll(function () {
     if (~~wWidth > 768) {
         var scroH = $(document).scrollTop();
         $("nav").css("background", "rgba(255,255,255," + scroH / 48 + ")");
+        if (scroH >= 48) {
+            $("#tooltip>ul>li:last").removeClass("hidden");
+        } else {
+            $("#tooltip>ul>li:last").addClass("hidden");
+        }
     }
 });
 
+//手机菜单动画
 $(document).on("click", ".menubtn", function (e) {
-    $(".phonemenu ul").toggle();
-})
+    $(".phonemenu ul").animate({ height: "toggle" });
+
+});
+
+//控制toopit
+$(document).on("click", "#tooltip>ul>li:last", function (e) {
+    $(document).scrollTop(0);
+});
+$(document).on("click", "#tooltip>ul>li:first", function (e) {
+    $(this).children("ul").animate({ width: "toggle" }, 200);
+});
 
 $(function () {
-
+    $('[data-toggle="tooltip"]').tooltip();
 });
