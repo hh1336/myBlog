4516a611f0c43644967481b5138c7000a1bb733a
mayu <1435473873@qq.com>
2019-05-17 16:36:23 +0800

    笔记标签管理完成

---
+++ Frame.Application/Interfaces/INoteLeableService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Frame.Application.Dtos.LabelManager;
+using Frame.ApplicationCore.Common;
+using Frame.Core.Entitys;
+
+namespace Frame.Application.Interfaces
+{
+    public interface INoteLeableService
+    {
+        /// <summary>
+        /// 获取所有数据作为表格
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        Task<IPageList<Classify>> GetAll(SerachLabelDtoInput dto);
+        
+        /// <summary>
+        /// 保存方法
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        Task<bool> Save(ClassifyDto data);
+
+        /// <summary>
+        /// 删除方法
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        Task<bool> Delete(Guid value);
+    }
+}
---
+++ Frame.Application/Services/NoteLeableService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Frame.Application.Dtos.LabelManager;
+using Frame.Application.Interfaces;
+using Frame.ApplicationCore.Bases;
+using Frame.ApplicationCore.Common;
+using Frame.Core.Entitys;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frame.Application.Services
+{
+    public class NoteLeableService : INoteLeableService
+    {
+        private readonly IRepository<Classify, Guid> _classifyRepository;
+        private readonly IMapper _mapper;
+
+        public NoteLeableService(IRepository<Classify, Guid> classifyRepository, IMapper mapper)
+        {
+            _classifyRepository = classifyRepository;
+            _mapper = mapper;
+        }
+
+
+
+        public async Task<IPageList<Classify>> GetAll(SerachLabelDtoInput dto)
+        {
+            var result = _classifyRepository.Where(s => s.AdminMenuId == Guid.Parse("eb08f902-bab2-4d6c-e64f-08d6d83d314d"));
+
+            if (!string.IsNullOrEmpty(dto.labelName))
+            {
+                result = result.Where(s => s.Name.Contains(dto.labelName));
+            }
+
+            return await result.Sort(dto.field, dto.order).ToPageList(dto.limit.Value, dto.page.Value);
+        }
+
+        public async Task<bool> Save(ClassifyDto data)
+        {
+            if (data.ID == Guid.Empty)
+            {
+                var entity = _mapper.Map<Classify>(data);
+                await _classifyRepository.InsertAsync(entity);
+            }
+            else
+            {
+                var entity = await _classifyRepository.FirstOrDefaultAsync(s => s.ID == data.ID);
+                if (entity == null) return false;
+                await _classifyRepository.UpdateAsync(_mapper.Map(data, entity));
+            }
+            return await _classifyRepository.CommitAsync() > 0;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            await _classifyRepository.DeleteAsync(s => s.ID == id);
+            return await _classifyRepository.CommitAsync() > 0;
+        }
+    }
+}
